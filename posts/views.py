e75d61ae1df615d245059ac6344915187d858837
santuzzxooy <santiagorochav@gmail.com>
2023-05-23 12:42:55 -0500

    prueba_listview-corregido

--- posts/views.py
+++ posts/views.py
@@ -3,12 +3,12 @@ from posts.models import Post
 from django.views.generic import ListView
 # Create your views here.
 
-def index(request):
-    return render(request,'posts/index.html')
+# def index(request):
+#     return render(request,'posts/index.html')
 
-# class index(ListView):
-#     model = Post
-#     template_name = 'posts/index.html'
+class index(ListView):
+    model = Post
+    template_name = 'posts/index.html'
 
 def about(request):
     return render(request,'posts/about.html')
