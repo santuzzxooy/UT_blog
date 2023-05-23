d954ab144b6a50d497f1ff7f2b84f4e6b5ae78c6
santuzzxooy <santiagorochav@gmail.com>
2023-05-23 12:16:25 -0500

    prueba_listview

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
-from django.views.generic import View
+from models import Post
+from django.views.generic import ListView
 # Create your views here.
 
-def index(request):
-    return render(request,'posts/index.html')
+class index(ListView):
+    model = Post
+    template_name = 'posts/index.html'
 
 def about(request):
     return render(request,'posts/about.html')
