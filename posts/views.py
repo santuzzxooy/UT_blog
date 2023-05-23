e43ebbe77d4bafee2b8f31bded9301f1a56f32f0
santuzzxooy <santiagorochav@gmail.com>
2023-05-23 00:34:44 -0500

    prueba 1

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from django.views.generic import View
+from django.http import HttpResponse
 # Create your views here.
+
+def prueba(request):
+    return HttpResponse("Esto es una prueba")
