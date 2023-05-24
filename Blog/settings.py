15e99384416f5010c4b0334d95ed4cd8ee5a89fa
santuzzxooy <santiagorochav@gmail.com>
2023-05-23 23:47:00 -0500

    admin1

--- Blog/settings.py
+++ Blog/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
