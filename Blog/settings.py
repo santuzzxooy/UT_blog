23e2fc549a5ad98c7982cf74f92b31e075190cf9
santuzzxooy <santiagorochav@gmail.com>
2023-06-09 10:46:31 -0500

    railway

--- Blog/settings.py
+++ Blog/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '3gsrQq0XfS5SyMCmuSA9',
+        'HOST': 'containers-us-west-75.railway.app',
+        'PORT': '7647',
     }
 }
 
