64ff154efbc3b774c867fd522ed47dca20f03d59
santuzzxooy <santiagorochav@gmail.com>
2023-06-09 14:38:26 -0500

    q_pendejo

--- Blog/urls.py
+++ Blog/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index.as_view(), name='index'),
-    path('', views.topic.as_view(), name='topic'),
+    path('topic', views.topic.as_view(), name='topic'),
     path('about', views.about, name='about'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
