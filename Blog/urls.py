26da8e9fd558d8e830ca54612296c2991514b25d
santuzzxooy <santiagorochav@gmail.com>
2023-06-09 12:07:08 -0500

    probando_data2

--- Blog/urls.py
+++ Blog/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from posts import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index.as_view(), name='index'),
     path('about', views.about, name='about'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
