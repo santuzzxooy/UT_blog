4c9b8568258ab3436b4ff43ce9df5a0ff43cc738
santuzzxooy <santiagorochav@gmail.com>
2023-06-10 00:41:04 -0500

    publicaciones2

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,42 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
 class Topics(models.Model):
-    title = models.CharField(blank=False, max_length=70)
-    sub = models.CharField(blank=False, max_length=200)
-    image = models.ImageField(blank=False, upload_to='images/')
+    title = models.CharField(blank=False,
+                             max_length=70)
+    
+    sub = models.CharField(blank=False,
+                           max_length=200)
+    
+    image = models.ImageField(blank=False,
+                              upload_to='images/')
+
+    slug = models.SlugField(max_length=255,
+                            blank=True,
+                            null=True,
+                            unique=True)
+
+    def __str__(self):
+        return self.title
+    
+        # OBTENER UN SLUG UNICO
+    def _get_unique_slug(self):
+        slug = slugify(self.title)
+        unique_slug = slug
+        num = 1
+        while Topics.objects.filter(slug=unique_slug).exists():
+            unique_slug = '{}-{}'.format(slug, num)
+            num += 1
+        return unique_slug
+    
+    def save(self, *args, **kwargs):  
+        if not self.slug:
+            self.slug = self._get_unique_slug()
+        super().save(*args, *kwargs)
+
+    
 
 class Post(models.Model):
     title = models.CharField(max_length=70)
@@ -20,3 +51,7 @@ class Post(models.Model):
     timage = models.ImageField(null=True, blank=True, upload_to='images/')
     date = models.DateField(auto_now_add=False)
     bibliography = models.CharField(max_length=250)
+    url = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from posts.models import Post, Topics
 from django.views.generic import ListView
 # Create your views here.
@@ -16,3 +16,10 @@ class topic(ListView):
 
 def about(request):
     return render(request,'posts/about.html')
+
+
+def topic_post(request, slug):
+    if (Topics.objects.filter(slug=slug)):
+        post = Post.objects.filter(topic__slug=slug)
+        context={'post':post}
+        return render(request, 'posts/topic_post.html', context)
