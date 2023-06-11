f3676f24736e3423b5949bb067dc0842ef72b0db
santuzzxooy <santiagorochav@gmail.com>
2023-06-11 00:41:43 -0500

    static2

--- Blog/settings.py
+++ Blog/settings.py
@@ -125,14 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR/'static')
 
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR/'media')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '/static/'),
     )
 
 # Default primary key field type
