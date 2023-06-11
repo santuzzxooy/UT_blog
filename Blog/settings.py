fba9f3fe29d73c32ee5705e4d7fa7d0e627e91a9
santuzzxooy <santiagorochav@gmail.com>
2023-06-11 00:39:11 -0500

    static

--- Blog/settings.py
+++ Blog/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR/'media')
 
