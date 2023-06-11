5173d12be83bd880e8780c7fb7e197b40cd25b6d
santuzzxooy <santiagorochav@gmail.com>
2023-06-11 00:24:46 -0500

    title

---
+++ posts/decorators.py
@@ -0,0 +1,9 @@
+class PageTitleMixin(object):  
+    def get_page_title(self, context):
+        return getattr(self, "page_title", "Default Page Title")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page_title"] = self.get_page_title(context)
+
+        return context
--- posts/views.py
+++ posts/views.py
@@ -2,20 +2,25 @@ from django.shortcuts import render
 from posts.models import Post, Topics
 from django.views.generic import ListView
 # Create your views here.
+from .decorators import PageTitleMixin
 
 # def index(request):
 #     return render(request,'posts/index.html')
 
-class index(ListView):
+class index(PageTitleMixin, ListView):
     model = Post
+    page_title = 'Home'
     template_name = 'posts/index.html'
 
-class topic(ListView):
+class topic(PageTitleMixin, ListView):
     model = Topics
+    page_title = 'Topics'
     template_name = 'posts/topics.html'
 
 def about(request):
-    return render(request,'posts/about.html')
+    page_title = 'About'
+    return render(request,'posts/about.html',{
+                  'page_title':page_title})
 
 
 def topic_post(request, title):
